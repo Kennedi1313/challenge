4c56486ac08425ea7bcdd00f23a0db60f82f8aef
kennedi1313 <francisco_kennedi@hotmail.com>
2022-02-05 22:06:17 -0300

    <green flag>

--- src/service/ReturnNthCharacterUppercaseService.java
+++ src/service/ReturnNthCharacterUppercaseService.java
@@ -5,6 +5,7 @@ import java.util.stream.IntStream;
 
 public class ReturnNthCharacterUppercaseService {
     public String returnNthCharacterUppercase(String givenText, int givenN) {
+        if(givenN <= 0) return "";
         return IntStream.range(0, givenText.length())
                 .filter(i -> (i+1) % givenN == 0)
                 .filter(i -> Character.isUpperCase(givenText.charAt(i)))
